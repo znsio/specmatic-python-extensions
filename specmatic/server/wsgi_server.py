16990ba31b255c674ba38e6be122f5a1786b1895
jaydeep <jaydeepk@gmail.com>
2023-05-23 13:08:07 +0530

    WSGI Server thread will signal when the web server is started.

--- specmatic/server/wsgi_server.py
+++ specmatic/server/wsgi_server.py
@@ -1,8 +1,10 @@
-from specmatic.server.server_thread import ServerThread
+import threading
+
+from specmatic.server.wsgi_server_thread import WSGIServerThread
 
 
 class WSGIServer:
-    server: ServerThread = None
+    server: WSGIServerThread = None
 
     def __init__(self, app, host: str, port: int):
         self.app = app
@@ -10,8 +12,9 @@ class WSGIServer:
         self.port = port
 
     def start(self):
-        self.server = ServerThread(self.app, self.host, self.port)
+        self.server = WSGIServerThread(self.app, self.host, self.port)
         self.server.start()
+        self.server.wait_for_start()
 
     def stop(self):
         self.server.shutdown()
--- specmatic/server/wsgi_server_thread.py
+++ specmatic/server/wsgi_server_thread.py
@@ -4,19 +4,27 @@ from werkzeug.serving import make_server
 import logging
 
 
-class ServerThread(threading.Thread):
+class WSGIServerThread(threading.Thread):
 
     def __init__(self, app, host: str, port: int):
         threading.Thread.__init__(self)
         logger = logging.getLogger("werkzeug")
         logger.addHandler(logging.StreamHandler(sys.stdout))
+        print("Starting web server")
         self.server = make_server(host, port, app)
         self.server.log_startup()
         self.ctx = app.app_context()
         self.ctx.push()
+        self.start_event = threading.Event()
+
+    def wait_for_start(self):
+        self.start_event.wait()
 
     def run(self):
+        print("Web server started")
+        self.start_event.set()
         self.server.serve_forever()
+
 
     def shutdown(self):
         self.server.shutdown()
