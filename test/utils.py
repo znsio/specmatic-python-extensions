9727734d456a98853b978bf23d68bb581cd59ca7
jaydeep <jaydeepk@gmail.com>
2023-05-31 15:50:28 +0530

    Specified absolute path to version.py in tests

--- test/utils.py
+++ test/utils.py
@@ -6,5 +6,5 @@ from build_utils import get_version, download_specmatic_jar
 def download_specmatic_jar_if_does_not_exist():
     specmatic_jar_file = Path(get_project_root() + '/specmatic/core/specmatic.jar')
     if not specmatic_jar_file.is_file():
-        version = get_version('./../specmatic/version.py')
+        version = get_version(get_project_root() + '/specmatic/version.py')
         download_specmatic_jar(version['__specmatic_version__'])
