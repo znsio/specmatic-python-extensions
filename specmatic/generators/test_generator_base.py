c0c338e931a13f1dc6ea1453b2c8a9224240c47d
Sufiyan <StarKhan6368@gmail.com>
2024-06-16 20:50:03 +0530

    Fix the Sanic adapter and test reporting. - Refactored the Sanic adapter to support regex route patterns. - Fixed test reporting by utilizing unique serial generation from JUnit XML files.

--- specmatic/generators/test_generator_base.py
+++ specmatic/generators/test_generator_base.py
@@ -1,15 +1,23 @@
+import re
 import xml.etree.ElementTree as ET
-
+from typing import Callable, Type
 
 class TestGeneratorBase:
+    pattern = re.compile(r"[\s\S]*dynamic-test:#(\d+)[\s\S]*Scenario: (.*)\n[\s\S]*")
     @staticmethod
-    def generate_tests(junit_report_path, test_class, passing_test_fn, failing_test_fn):
-        root = ET.parse(junit_report_path).getroot()
-        for testcase in root.iter('testcase'):
-            scenario = testcase.find('system-out').text.split('display-name:')[1].strip()
-            test_name = "test_" + scenario
-            failure = testcase.find('failure')
-            if failure is None:
-                setattr(test_class, test_name, passing_test_fn())
-            else:
-                setattr(test_class, test_name, failing_test_fn(failure.get('message') + failure.text))
+    def generate_tests(junit_report_path: str, test_class: Type, passing_test_fn: Callable, failing_test_fn: Callable) -> None:
+        try:
+            root = ET.parse(junit_report_path).getroot()
+            for testcase in root.iter('testcase'):
+                test_case_info = re.match(TestGeneratorBase.pattern, testcase.find('system-out').text) # type: ignore
+                if test_case_info is None:
+                    raise ValueError("Invalid test case information")
+                unique_id, scenario = test_case_info.groups()
+                test_name = f"test_scenario_{unique_id} {scenario}"
+                failure_message = testcase.findtext('failure')
+                if failure_message is None:
+                    setattr(test_class, test_name, passing_test_fn())
+                else:
+                    setattr(test_class, test_name, failing_test_fn(failure_message))
+        except ET.ParseError as e:
+            raise ValueError("Invalid XML file") from e
