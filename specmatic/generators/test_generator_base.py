e25dda3c68de78e06e21a5ffafb287744c971ac0
Yogesh Ananda Nikam <yogeshnikam671@gmail.com>
2024-06-19 12:24:21 +0530

    Sort the testcases based on the unique id before logging them in the console

--- specmatic/generators/test_generator_base.py
+++ specmatic/generators/test_generator_base.py
@@ -3,21 +3,26 @@ import xml.etree.ElementTree as ET
 from typing import Callable, Type
 
 class TestGeneratorBase:
-    pattern = re.compile(r"[\s\S]*dynamic-test:#(\d+)[\s\S]*Scenario: (.*)\n[\s\S]*")
+    pattern = re.compile(r"[\s\S]*dynamic-test:#(\d+)[\s\S]*")
     @staticmethod
     def generate_tests(junit_report_path: str, test_class: Type, passing_test_fn: Callable, failing_test_fn: Callable) -> None:
+        testcases = []
         try:
             root = ET.parse(junit_report_path).getroot()
             for testcase in root.iter('testcase'):
                 test_case_info = re.match(TestGeneratorBase.pattern, testcase.find('system-out').text) # type: ignore
                 if test_case_info is None:
                     raise ValueError("Invalid test case information")
-                unique_id, scenario = test_case_info.groups()
-                test_name = f"test_scenario_{unique_id} {scenario}"
+                unique_id = test_case_info.groups()[0]
+                test_name = f"test [{unique_id}] {testcase.get("name")}"
                 failure_message = testcase.findtext('failure')
                 if failure_message is None:
-                    setattr(test_class, test_name, passing_test_fn())
+                    testcases.append((int(unique_id), test_name, passing_test_fn))
                 else:
-                    setattr(test_class, test_name, failing_test_fn(failure_message))
+                    testcases.append((int(unique_id), test_name, failing_test_fn))
         except ET.ParseError as e:
             raise ValueError("Invalid XML file") from e
+
+        testcases.sort(key = lambda testcase : testcase[0])
+        for (unique_id, test_name, test_fn) in testcases:
+            setattr(test_class, test_name, test_fn())
