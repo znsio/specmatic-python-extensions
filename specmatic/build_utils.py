3f6d86c1d6f53fdc4f9a938065e06e192ca44755
Sufiyan <StarKhan6368@gmail.com>
2024-07-19 20:51:30 +0530

    Upgrade Specmatic version to 2.0.0
    - Update Specmatic Executable to 2.0.0
    - Change maven URL to `io/specmatic`.

--- specmatic/build_utils.py
+++ specmatic/build_utils.py
@@ -4,7 +4,7 @@ from specmatic.utils import get_project_root
 
 
 def download_specmatic_jar(version):
-    file_url = f'https://repo1.maven.org/maven2/in/specmatic/specmatic-executable/{version}/specmatic-executable-{version}-all.jar'
+    file_url = f'https://repo1.maven.org/maven2/io/specmatic/specmatic-executable/{version}/specmatic-executable-{version}-all.jar'
     file_path = get_project_root() + '/specmatic/core/specmatic.jar'
     print(f"Downloading specmatic jar from: {file_url}")
     urllib.request.urlretrieve(file_url, file_path)
--- specmatic/version.py
+++ specmatic/version.py
@@ -1,2 +1,2 @@
-__version__ = '1.3.38'
-__specmatic_version__ = '1.3.38'
+__version__ = '2.0.0'
+__specmatic_version__ = '2.0.0'
